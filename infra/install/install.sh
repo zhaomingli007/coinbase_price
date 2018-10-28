a38ed4d4b0f2b3a5a36d096f9e1fb4fc8023f47d
Zhao Mingli <minglee.zhao@gmail.com>
2018-10-28 16:28:10 +0800

    Fix talend script permission issue

--- infra/install/install.sh
+++ infra/install/install.sh
@@ -6,9 +6,9 @@ docker pull zhaomingli/pyscala:coinbase
 
 #### Run docker containers
 #Run Mysql container
-sudo docker run --name mysql -e MYSQL_ROOT_PASSWORD=coinBase$ -d zhaomingli/mysql8:coinbase
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=coinBase$ -d zhaomingli/mysql8:coinbase
 #Run data pipeline container
-docker run --name pipeline --link mysql:coinbase-mysql -t pyscala:coinbase
+docker run --name pipeline --link mysql:coinbase-mysql -t -d pyscala:coinbase
 docker exec -d pipeline bash tools/run_pipeline.sh
 #Run scala+play container
 docker run --name play --link mysql:coinbase-mysql -t pyscala:coinbase
--- infra/install/run_pipeline.sh
+++ infra/install/run_pipeline.sh
@@ -5,6 +5,7 @@ git clone https://github.com/zhaomingli007/coinbase_price.git
 #Extract and Talend data pipeline scripts
 mkdir talend_runtime
 unzip coinbase_price/data_ingestion/batch/Talend_run/main_pipeline_0.1.zip -d ./talend_runtime
+chmod -R +rx ./talend_runtime
 talend_runtime/main_pipeline/main_pipeline_run.sh -> main_pipeline_run.log
 
 #schedule
