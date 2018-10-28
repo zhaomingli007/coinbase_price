4382cbabe1ec7a1f9cc2969ffef3bb69bc34f4e0
Zhao Mingli <minglee.zhao@gmail.com>
2018-10-28 18:25:20 +0800

    create table if not exist for price table

--- infra/install/install.sh
+++ infra/install/install.sh
@@ -6,7 +6,10 @@ docker pull zhaomingli/pyscala:coinbase
 
 #### Run docker containers
 #Run Mysql container
-docker run --name mysql -e MYSQL_ROOT_PASSWORD=coinBase$ -d zhaomingli/mysql8:coinbase
+docker run --name mysql -v /Users/zhao/codebase/coinbase_price/infra/install/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD='coinBase$' -d zhaomingli/mysql8:coinbase
+#Create a database
+docker exec -d mysql mysql -u root -p'coinBase$' -h localhost -e 'create database coinbase'
+
 #Run data pipeline container
 docker run --name pipeline --link mysql:coinbase-mysql -t -d pyscala:coinbase
 docker exec -d pipeline bash tools/run_pipeline.sh
